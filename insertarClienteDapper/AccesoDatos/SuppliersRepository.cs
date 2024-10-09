e6b1cdb6dd15a759cb593be741eb99adef7f5d49
Jeremy Fuentes <jeremyfuentes201@gmail.com>
2024-10-08 21:19:18 -0600

    base de dapper

---
+++ insertarClienteDapper/AccesoDatos/SuppliersRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class SuppliersRepository
+    {
+        public List<Suppliers> ObtenerTodos()
+        {
+            using (var conexion = Database.GetSqlConnection())
+            {
+                String SelcetAll = "";
+                SelcetAll = SelcetAll + "SELECT [SupplierID] " + "\n";
+                SelcetAll = SelcetAll + "      ,[CompanyName] " + "\n";
+                SelcetAll = SelcetAll + "      ,[ContactName] " + "\n";
+                SelcetAll = SelcetAll + "      ,[ContactTitle] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Address] " + "\n";
+                SelcetAll = SelcetAll + "      ,[City] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Region] " + "\n";
+                SelcetAll = SelcetAll + "      ,[PostalCode] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Country] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Phone] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Fax] " + "\n";
+                SelcetAll = SelcetAll + "      ,[HomePage] " + "\n";
+                SelcetAll = SelcetAll + "  FROM [dbo].[Suppliers]";
+
+                var categoria = conexion.Query<Suppliers>(SelcetAll).ToList();
+                return categoria;
+            }
+        }
+
+    }
+}
---
+++ insertarClienteDapper/insertarClienteDapper/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace insertarClienteDapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCategoria = new System.Windows.Forms.DataGridView();
+            this.btnObtenerTodo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCategoria
+            // 
+            this.dgvCategoria.AllowUserToAddRows = false;
+            this.dgvCategoria.AllowUserToDeleteRows = false;
+            this.dgvCategoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategoria.Location = new System.Drawing.Point(27, 31);
+            this.dgvCategoria.Name = "dgvCategoria";
+            this.dgvCategoria.ReadOnly = true;
+            this.dgvCategoria.RowHeadersWidth = 51;
+            this.dgvCategoria.RowTemplate.Height = 24;
+            this.dgvCategoria.Size = new System.Drawing.Size(1004, 337);
+            this.dgvCategoria.TabIndex = 0;
+            // 
+            // btnObtenerTodo
+            // 
+            this.btnObtenerTodo.Location = new System.Drawing.Point(438, 487);
+            this.btnObtenerTodo.Name = "btnObtenerTodo";
+            this.btnObtenerTodo.Size = new System.Drawing.Size(139, 48);
+            this.btnObtenerTodo.TabIndex = 1;
+            this.btnObtenerTodo.Text = "Obtener Todo";
+            this.btnObtenerTodo.UseVisualStyleBackColor = true;
+            this.btnObtenerTodo.Click += new System.EventHandler(this.btnObtenerTodo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 673);
+            this.Controls.Add(this.btnObtenerTodo);
+            this.Controls.Add(this.dgvCategoria);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCategoria;
+        private System.Windows.Forms.Button btnObtenerTodo;
+    }
+}
+
